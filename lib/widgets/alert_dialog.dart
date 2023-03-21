664702106037d67c2c3189005e4bda77c6d09a27
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-03-21 23:16:16 +0530

    changes

--- lib/widgets/alert_dialog.dart
+++ lib/widgets/alert_dialog.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class AlertDialogWidget extends StatefulWidget {
+class AlertDialogWidget extends StatelessWidget {
   const AlertDialogWidget({super.key});
 
-  @override
-  State<AlertDialogWidget> createState() => _AlertDialogWidgetState();
-}
-
-class _AlertDialogWidgetState extends State<AlertDialogWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
