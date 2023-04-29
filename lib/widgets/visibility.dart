162a777f2ada4e1a2d50fd38e5f6b23ec4607650
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-04-29 21:24:21 +0530

    new file

---
+++ lib/widgets/visibility.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class VisibilityWidget extends StatefulWidget {
+  const VisibilityWidget({super.key});
+
+  @override
+  State<VisibilityWidget> createState() => _VisibilityWidgetState();
+}
+
+class _VisibilityWidgetState extends State<VisibilityWidget> {
+  //
+
+  bool isVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Visibility Widget'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isVisible = !isVisible;
+                });
+              },
+              child: Text(isVisible ? 'Show image' : 'Show image'),
+            ),
+            SizedBox(height: 10),
+            Visibility(
+              visible: isVisible,
+              child: Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
