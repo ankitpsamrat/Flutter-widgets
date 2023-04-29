507e9facea6ccfc3bb895bea4f13e32549e7898e
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-04-29 21:24:30 +0530

    new file

---
+++ lib/widgets/will_pop_scope.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WillPopScopeWidget extends StatelessWidget {
+  const WillPopScopeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WillPopScope Widget'),
+        elevation: 0,
+      ),
+    );
+  }
+}
