129d22ab4998405f86863ff9e8599d6a19d6f9d6
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-10-26 23:17:13 +0530

    changes

--- lib/widgets/text_form_field.dart
+++ lib/widgets/text_form_field.dart
@@ -1,14 +1,106 @@
 import 'package:flutter/material.dart';
 
 class TextFormFieldWidget extends StatelessWidget {
-  const TextFormFieldWidget({super.key});
+  TextFormFieldWidget({super.key});
+
+  //  to validate user input
+  final _formKey = GlobalKey<FormState>();
+
+  //  to take input from user
+  final textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text Form Field Widget'),
         elevation: 0,
+        title: const Text('Text Form Field Widget'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                padding: const EdgeInsets.only(left: 10),
+                child: TextFormField(
+                  controller: textController,
+                  // to auto validation
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  // to validate user input
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter name';
+                    }
+                    return null;
+                  },
+                  // to open keyboard automaticaly
+                  // autofocus: true,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    // to reduce hight
+                    isDense: true,
+                    hintText: 'Enter name',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        textController.clear();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const ConfirmationPage();
+                        },
+                      ),
+                    );
+                  }
+                },
+                child: const Text(
+                  'Go to next page',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ConfirmationPage extends StatelessWidget {
+  const ConfirmationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Welcome'),
+      ),
+      body: const Center(
+        child: Text('Welcome guys!'),
       ),
     );
   }
--- lib/widgets/will_pop_scope.dart
+++ lib/widgets/will_pop_scope.dart
@@ -7,8 +7,101 @@ class WillPopScopeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WillPopScope Widget'),
         elevation: 0,
+        title: const Text('Will Pop Scope Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'HomeView',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const ContentView();
+                    },
+                  ),
+                );
+              },
+              child: const Text(
+                'Go To ContentView',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContentView extends StatelessWidget {
+  const ContentView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final shouldPop = await showDialog<bool>(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Do you want to go back?'),
+              actionsAlignment: MainAxisAlignment.spaceBetween,
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                  child: const Text('Yes'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: const Text('No'),
+                ),
+              ],
+            );
+          },
+        );
+        return shouldPop!;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Content View'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Text(
+                'This is the Content View',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
