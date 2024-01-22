7739cb2002bd96a184306ae8a7c7570a9a97d49a
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-01-22 23:23:20 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:widgets/widgets/silver_app_bar.dart';
 
 void main() {
+  // error handling method using error widget
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Material(
+      child: Container(
+        color: Colors.purple,
+        alignment: Alignment.center,
+        // also can add icon with text for a better look
+        child: Text(
+          kReleaseMode
+              ? 'Oops... something went wrong!'
+              : details.exceptionAsString(),
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  };
+  
   runApp(const MyApp());
 }
 
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -2,28 +2,29 @@ import 'package:flutter/material.dart';
 
 class DropDownWidget extends StatefulWidget {
   const DropDownWidget({Key? key}) : super(key: key);
+
   @override
   State<DropDownWidget> createState() => _DropDownWidgetState();
 }
 
 class _DropDownWidgetState extends State<DropDownWidget> {
+  //
+
   String dropdownValue = 'Dog';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: ...,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('DropDown Widget'),
+      ),
       body: Center(
         child: Column(
           children: [
-            SizedBox(
-              height: 50,
-            ),
-            // Step 2.
+            SizedBox(height: 50),
             DropdownButton<String>(
-              // Step 3.
               value: dropdownValue,
-              // Step 4.
               items: <String>['Dog', 'Cat', 'Tiger', 'Lion']
                   .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
@@ -34,20 +35,20 @@ class _DropDownWidgetState extends State<DropDownWidget> {
                   ),
                 );
               }).toList(),
-              // Step 5.
               onChanged: (String? newValue) {
                 setState(() {
                   dropdownValue = newValue!;
                 });
               },
             ),
-            SizedBox(
-              height: 20,
-            ),
+            SizedBox(height: 20),
             Text(
               'Selected Value: $dropdownValue',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            )
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
